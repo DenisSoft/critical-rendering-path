da459ace38662cb6ca9f0990e41703852ccaf62e
DenisSoft <bdv@bk.ru>
2017-09-14 21:55:21 +0300

    test

---
+++ js/perfmatters.js
@@ -0,0 +1,2 @@
+Cache-control: privet,max-age=31536000
+function logCRP(){var a=window.performance.timing,b=a.domContentLoadedEventStart-a.domLoading;a=a.domComplete-a.domLoading;document.getElementById("crp-stats").textContent="DCL: "+b+"ms, onload: "+a+"ms"}window.addEventListener("load",function(a){logCRP()});
